6a4a9b04fcc5ef6d7cb60d2ef1475f957fd75422
Theos <david_ramirezz@hotmail.com>
2022-01-13 13:09:40 -0600

    Coneccion con base de datos

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
+
+
+if (process.env.NODE_ENV === "production") {
+    require('dotenv').config();
+}
+
 
 const app = express();
 
@@ -14,5 +21,12 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
+const URI = process.env.DATABASE_URL;
+console.log(URI);
+//db conecct
+mongoose.connect("mongodb://localhost/mybrary");
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', MONGO_URI => console.log('Connected to Database'))
 
 app.listen(process.env.PORT || 3000);
