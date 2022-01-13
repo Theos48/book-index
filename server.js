b69149c87b6e8223b7b0ff5d45b2b6d63ea020af
Theos <david_ramirezz@hotmail.com>
2022-01-13 15:58:10 -0600

    Conflicto heroku env

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
-const { process_params } = require('express/lib/router');
-
-// require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
-
 
 const app = express();
 
